3ea5f0efeea9fe193947a7dfdc07703a80ce07ee
Benjamin Poon <benjamintpoon@gmail.com>
2017-05-08 00:16:56 -0500

    request summoner data from form on homepage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const request = require('request')
 
-/* GET home page. */
+const api_key = '98b67977-fba4-4435-88a0-461acf65bb34'
+const baseUrl = `https://na1.api.riotgames.com/lol/`
+
+const urls = {
+  summoner: {
+    byName: (summonerName) => {
+      return `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${summonerName}?api_key=${api_key}`
+    }
+  }
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'RiotApp' });
-});
+  res.render('index', { title: 'RiotApp' })
+})
 
-module.exports = router;
+router.post('/summoner', function(req, res, next) {
+  const summonerName = req.body['summoner-name']
+  url = urls.summoner.byName(summonerName)
+  request(url, {}, function (err, rs, body) {
+    if (err) console.log(err)
+    console.log(url)
+    res.send(body)
+  })
+})
+
+module.exports = router
